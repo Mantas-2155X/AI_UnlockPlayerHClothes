d64f7caeaab30f5c10a055b4acaa3b2fabddaa5a
Mantas <syenergyy@gmail.com>
2019-11-26 12:06:16 +0200

    Add version, only run in main game

--- AI_UnlockPlayerHClothes/Properties/AssemblyInfo.cs
+++ AI_UnlockPlayerHClothes/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AI_UnlockPlayerHClothes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("2155X aka Mantas-2155X")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
 [assembly: AssemblyProduct("AI_UnlockPlayerHClothes")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Mantas-2155X aka 2155X Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion(AI_UnlockPlayerHClothes.AI_UnlockPlayerHClothes.VERSION)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
